fca380ede323ce61d0f42723dd2251a8327ce000
bradforbush <bradforbush@gmail.com>
2020-12-06 11:29:47 -0700

    Update configuration.js

--- src/configuration.js
+++ src/configuration.js
@@ -1,8 +1,8 @@
 const config = {
-  header: "Header",
+  header: "Welcome to the 42nd Ward",
   headerBackground: "##566573",
   bodyBackground: "#ffffff",
-  title: "Welcome to the 42nd Ward",
+  title: "Commitment to serving the Savior",
   subTitle: "Inviting Others to Come Unto Christ",
   airtable: "https://airtable.com/embed/shrdi24ocwoEeXx6d?backgroundColor=red&viewControls=on",
   googleAnalytics: "UA-XXXXXXXX-X"
