f692f753c3f5c9d45c1b9cca736cd3c2c83bc6f9
bradforbush <bradforbush@gmail.com>
2020-12-06 22:44:49 -0700

    Update configuration.js

--- src/configuration.js
+++ src/configuration.js
@@ -1,6 +1,6 @@
 const config = {
   header: "Welcome to the 42nd Ward",
-  headerBackground: "##566573",
+  headerBackground: "#566573",
   bodyBackground: "#ffffff",
   title: "Commitment to serving the Savior",
   subTitle: "Inviting Others to Come Unto Christ",
